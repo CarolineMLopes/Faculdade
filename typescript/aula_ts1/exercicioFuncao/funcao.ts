cec4a331ad627413ccde3ebea1019b57a905d8f2
Caroline Marcon Lopes <79430964+CarolineMLopes@users.noreply.github.com>
2025-05-24 11:02:00 -0300

    TypeScript

---
+++ typescript/aula_ts1/exercicioFuncao/funcao.ts
@@ -0,0 +1,19 @@
+function soma(a: number, b:number): number{
+    return a + b;
+}
+
+function saudacao(nome: string, titulo?: string): string{
+    if(titulo){
+        return `Olá, ${titulo} ${nome}!`;
+    }
+
+    return `Olá, $(nome)`;
+}
+
+console.log(saudacao("Maria"));
+console.log(saudacao("Silva", "Dr"));
+
+function criarElemento(tipo: string, texto:string, classes: string = ""): string{
+    return `<${tipo} class="${classes}"> ${texto}</${tipo}>`
+}
+
